2f29be3f2be37f77d315ea051ed2b923a05a443c
ruilin <zruilin@126.com>
2019-01-03 11:04:56 +0800

    google place search

--- app/src/main/java/com/muyu/mapnote/map/search/PoiSearchController.java
+++ app/src/main/java/com/muyu/mapnote/map/search/PoiSearchController.java
@@ -10,6 +10,7 @@ import com.google.android.gms.location.places.PlaceDetectionClient;
 import com.google.android.gms.location.places.PlaceLikelihood;
 import com.google.android.gms.location.places.PlaceLikelihoodBufferResponse;
 import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.mapbox.mapboxsdk.maps.MapView;
@@ -19,6 +20,7 @@ import com.muyu.minimalism.framework.util.MLog;
 import com.muyu.minimalism.framework.util.Msg;
 
 public class PoiSearchController extends MapPluginController {
+    private final static int M_MAX_ENTRIES = 10;
     private PlaceDetectionClient mPlaceDetectionClient;
 
     @Override
@@ -33,17 +35,35 @@ public class PoiSearchController extends MapPluginController {
         placeResult.addOnCompleteListener(new OnCompleteListener<PlaceLikelihoodBufferResponse>() {
             @Override
             public void onComplete(@NonNull Task<PlaceLikelihoodBufferResponse> task) {
-                try {
+                if (task.isSuccessful() && task.getResult() != null) {
                     PlaceLikelihoodBufferResponse likelyPlaces = task.getResult();
+
+                    // Set the count, handling cases where less than 5 entries are returned.
+                    int count;
+                    if (likelyPlaces.getCount() < M_MAX_ENTRIES) {
+                        count = likelyPlaces.getCount();
+                    } else {
+                        count = M_MAX_ENTRIES;
+                    }
+
+                    int i = 0;
                     for (PlaceLikelihood placeLikelihood : likelyPlaces) {
-                        MLog.e(String.format("Place '%s' has likelihood: %g",
-                                placeLikelihood.getPlace().getName(),
-                                placeLikelihood.getLikelihood()));
-                        Msg.showDebug("xxx" + placeLikelihood.getPlace().getName());
+                        // Build a list of likely places to show the user.
+                        String mLikelyPlaceNames = (String) placeLikelihood.getPlace().getName();
+                        String mLikelyPlaceAddresses = (String) placeLikelihood.getPlace()
+                                .getAddress();
+                        String mLikelyPlaceAttributions = (String) placeLikelihood.getPlace()
+                                .getAttributions();
+                        LatLng mLikelyPlaceLatLngs = placeLikelihood.getPlace().getLatLng();
+                        MLog.d("Google place search: " + mLikelyPlaceNames + " : " + mLikelyPlaceAttributions + " : " + mLikelyPlaceAddresses);
+                        i++;
+                        if (i > (count - 1)) {
+                            break;
+                        }
                     }
                     likelyPlaces.release();
-                } catch (Exception e) {
-                    MLog.e(e.toString());
+                } else {
+                    MLog.e("Google place search error: " + task.getException());
                 }
             }
         });
