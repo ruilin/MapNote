b2d9a3e19fe9b07c7d5824a420965b39ff85f89e
ruilin <zruilin@126.com>
2019-01-21 12:26:36 +0800

    整理

--- minimalism/src/main/java/com/muyu/minimalism/view/BottomMenu.java
+++ minimalism/src/main/java/com/muyu/minimalism/view/BottomMenu.java
@@ -5,21 +5,18 @@ import android.app.Dialog;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
-
 import com.muyu.minimalism.R;
 
 public class BottomMenu {
 
     public static void show(Activity activity, String[] items, OnItemClickedListener listener) {
-        Dialog dialog = new Dialog(activity, R.style.ActionSheetDialogStyle);
-        //填充对话框的布局
+        Dialog dialog = new Dialog(activity, R.style.BottomMenuStyle);
+        // 填充对话框的布局
         View view = LayoutInflater.from(activity).inflate(R.layout.view_bottom_menu, null);
 
         ListView listView = view.findViewById(R.id.bottom_menu_list);
@@ -41,19 +38,17 @@ public class BottomMenu {
             }
         });
 
-        //将布局设置给Dialog
         dialog.setContentView(view);
-        //获取当前Activity所在的窗体
         Window dialogWindow = dialog.getWindow();
-        //设置Dialog从窗体底部弹出
+        // 设置Dialog从窗体底部弹出
         dialogWindow.setGravity(Gravity.BOTTOM);
-        //获得窗体的属性
+        // 获得窗体的属性
         WindowManager.LayoutParams lp = dialogWindow.getAttributes();
         lp.width = WindowManager.LayoutParams.MATCH_PARENT;
         lp.y = 0;//设置Dialog距离底部的距离
-//       将属性设置给窗体
+
         dialogWindow.setAttributes(lp);
-        dialog.show();//显示对话框
+        dialog.show();
     }
 
     public interface OnItemClickedListener {
