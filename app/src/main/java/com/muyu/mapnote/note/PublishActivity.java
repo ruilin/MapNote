eec1c91d81d2bf4638dcd7e34d9dd920b15a2b52
ruilin <zruilin@126.com>
2019-01-21 14:38:01 +0800

    take photo

--- app/src/main/java/com/muyu/mapnote/note/PublishActivity.java
+++ app/src/main/java/com/muyu/mapnote/note/PublishActivity.java
@@ -1,7 +1,19 @@
 package com.muyu.mapnote.note;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -17,6 +29,7 @@ import com.muyu.minimalism.view.imagebox.ZzImageBox;
 import com.wuhenzhizao.titlebar.widget.CommonTitleBar;
 
 public class PublishActivity extends BaseActivity {
+    ZzImageBox imageBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +63,7 @@ public class PublishActivity extends BaseActivity {
     }
 
     private void initImageBox() {
-        final ZzImageBox imageBox = (ZzImageBox) findViewById(R.id.zz_image_box);
+        imageBox = findViewById(R.id.zz_image_box);
         imageBox.setOnImageClickListener(new ZzImageBox.OnImageClickListener() {
 
             @Override
@@ -68,15 +81,76 @@ public class PublishActivity extends BaseActivity {
             @Override
             public void onAddClick() {
                 imageBox.addImage(null);
-                Log.d("ZzImageBox", "add clicked");
                 Log.d("ZzImageBox", "all images\n"+imageBox.getAllImages().toString());
                 BottomMenu.show(PublishActivity.this, new String[]{"拍照", "从相册选取"}, new BottomMenu.OnItemClickedListener() {
                     @Override
                     public void OnItemClicked(int position) {
-                        Msg.show("" + position);
+                        switch (position) {
+                            case 0:
+                                takePhoto();
+                                break;
+                            case 1:
+                                break;
+                        }
                     }
                 });
             }
         });
+    }
+
+    final int TAKE_PHOTO_REQUEST = 101;
+    Uri imageUri;
+    private void takePhoto() {
+        imageUri = createImageUri(this);
+        if (imageUri == null)
+            return;
+        Intent intent = new Intent();
+        intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);//如果不设置EXTRA_OUTPUT getData()  获取的是bitmap数据  是压缩后的
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            int checkCallPhonePermission = ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.CAMERA);
+            if (checkCallPhonePermission != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 222);
+                return;
+            } else {
+                startActivityForResult(intent, TAKE_PHOTO_REQUEST);
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case TAKE_PHOTO_REQUEST:
+                if (resultCode == RESULT_CANCELED) {
+                    delteImageUri(PublishActivity.this, imageUri);
+                    return;
+                }
+                imageBox.addImage(imageUri.getPath());
+                Msg.show(imageUri.getPath());
+                break;
+        }
+    }
+
+    private static Uri createImageUri(Activity activity) {
+        Uri uri = null;
+        if (ContextCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 111);
+        } else {
+            String name = "takePhoto" + System.currentTimeMillis();
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(MediaStore.Images.Media.TITLE, name);
+            contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, name + ".jpeg");
+            contentValues.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+            uri = activity.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+        }
+        return uri;
+    }
+
+    public static void delteImageUri(Context context, Uri uri) {
+        context.getContentResolver().delete(uri, null, null);
     }
 }
